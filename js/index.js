5ee98ee18cbdf87bcc791ad88a0bd4a30b6d9a89
zkohn <joaovitorcampos99@gmail.com>
2024-04-08 03:29:07 -0300

    final

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ import { convidados } from './nomes.js'
 
 window.addEventListener("load", () => {
     const urlParams = new URLSearchParams(window.location.search);
-    const bigParam = "name";
+    const bigParam = "arewweqeasdqwefsfdc2ah790feqcnh8oFBg4L0do1D02O24C3u9rmx23iww13321ws23jmUHJxro3nh790feqcnh8oFBDFsd86asdgggevvadwf";
     const nome = urlParams.get(bigParam);
     console.log(nome);
     if(convidados.includes(nome)){
         document.querySelector('.aceito').classList.toggle("hide");
         document.querySelector('.recusado').classList.toggle("hide");
+        document.querySelector('.nome').innerHTML = nome.toUpperCase();
     }
 })
